e75634b18a700ce46978234e560d1c023ebab0c1
Patrick Mennen <patrick.mennen@philips.com>
2021-10-22 16:17:02 +0200

    add more methods to TeamResolver

--- packages/backend/src/TeamResolver/TeamResolver.ts
+++ packages/backend/src/TeamResolver/TeamResolver.ts
@@ -1,21 +1,43 @@
-import {Ctx, FieldResolver, Int, Resolver, Root} from "type-graphql";
-import {Team} from "../../prisma/generated/type-graphql";
-import {Context} from "../index";
+import { Ctx, FieldResolver, Int, Resolver, Root } from 'type-graphql';
+import { Team } from '../../prisma/generated/type-graphql';
+import { Context } from '../index';
+import { PrismaClient } from '@prisma/client';
 
-@Resolver(of => Team)
+const numberOfTasksForTeamQuery = async (
+  prisma: PrismaClient,
+  teamId: number,
+  completed = false,
+) => {
+  return await prisma.task.count({
+    where: {
+      completed,
+      AND: {
+        Tasklist: {
+          teamId,
+        },
+      },
+    },
+  });
+};
+
+@Resolver((of) => Team)
 export class CustomTeamResolver {
-  @FieldResolver(type => Int, {nullable: false})
-  async totalNumberOfOpenTasks(@Root() team: Team,
-                               @Ctx() {prisma}: Context): Promise<number> {
+  @FieldResolver((type) => Int, { nullable: false })
+  async totalNumberOfTasks(@Root() team: Team, @Ctx() { prisma }: Context): Promise<number> {
     return await prisma.task.count({
       where: {
-        completed: false,
-        AND: {
-          Tasklist: {
-            teamId: team.id
-          }
-        }
+        Tasklist: {
+          teamId: team.id,
+        },
       },
-    })
+    });
+  }
+
+  async totalNumberOfClosedTasks(@Root() team: Team, @Ctx() { prisma }: Context): Promise<number> {
+    return await numberOfTasksForTeamQuery(prisma, team.id, true);
+  }
+
+  async totalNumberOfOpenTasks(@Root() team: Team, @Ctx() { prisma }: Context): Promise<number> {
+    return await numberOfTasksForTeamQuery(prisma, team.id);
   }
 }
